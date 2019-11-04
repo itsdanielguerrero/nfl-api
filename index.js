6d1bb07e38b0e28e7ad05c2786b4e4cf0e758f6d
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-11-03 20:19:10 -0500

    ADD 3 working GET handlers 4 homepage listing all team and displaying specific team

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const app = express()
 const teams = require('./teams.json')
 
 
+app.get ('/', (request, response) => {
+    response.send('This is our NFL WEB APP, I can get you any current nfl team you would like. \n Note: if you can\'t choose I can just display them all for you!')
+})
+
 app.get ('/teams', (request, response) => {
     response.send(teams)
 })
 
-app.get ('/teams/:teamID', (request, response) => {
-    let teamRequested = teams.filter((team) => {
-        return team.id = request.param.teamID
+app.get ('/teams/:filter', (request, response) => {
+    //response.send(request.params.filter)
+    
+    var teamRequested = teams.filter((team) => {
+        return parseInt(request.params.filter) === team.id || request.params.filter === team.abbreviation 
     })
-    response.send(teamRequested)
-})
 
-app.get ('/teams/:abbr', (request, response) => {
-    let teamRequested = teams.filter((team) => {
-        return team.abbreviation == request.param.abbr
-    })
-    response.send(teamRequested)
+    if (teamRequested.length){
+        response.send(teamRequested)
+    } else {
+        response.sendStatus(404)
+    }
+    
 })
 
 app.all('*', (request, response) => {
