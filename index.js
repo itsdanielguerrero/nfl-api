4cd39a2fa6868c230e8f6f62542f184b39b6a25a
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-11-04 22:54:53 -0500

    adding post-team-answer branch, and app.post handler

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const teams = require('./teams.json')
 
+function validatePost(body){
+    //must have location, mascot, abbreviation, conference, division
+    let { location, mascot, abbreviation, conference, division } = body
+    if(!location || !mascot || !abbreviation || !conference || !division){
+        return false
+    } else {
+        return true
+    }
+}
+
+app.use(bodyParser.json())
+
+app.post('/teams', (request, respond) => {
+    let newTeam = request.body || {}
+    newTeam.id = teams.length + 1
+    if(!validatePost(newTeam)){
+        respond.status(400).send("The following attributes are required: location, mascot, abbreviation, conference, division")
+    } else {
+        teams.push(newTeam)
+        respond.status(201).send(newTeam)
+    }
+
+})
 
 app.get ('/', (request, response) => {
     response.send('This is our NFL WEB APP, I can get you any current nfl team you would like. \n Note: if you can\'t choose I can just display them all for you!')
@@ -23,13 +47,13 @@ app.get ('/teams/:filter', (request, response) => {
     } else {
         response.sendStatus(404)
     }
-    
+     
 })
 
 app.all('*', (request, response) => {
     response.send('Oops, Did you find what your looking for?')
 })
 
-app.listen(1337, () => {
+app.listen(1338, () => {
     console.log('Server is up!')
 })
